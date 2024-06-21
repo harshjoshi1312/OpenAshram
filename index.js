a5d40b981125a3e8471bf711672e8f89db4206f6
harshjoshi1312 <joshiharsh1312@gmail.com>
2024-06-21 21:43:35 +0530

    basic setup completed

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const mongoose = require("mongoose");
+
+
+const dburl = process.env.MONGODB_URL;
+
+main()
+  .then(() => {
+    console.log("success");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(dburl);
+}
+
+
+app.set("view engine", "ejs");
+
+app.listen(port, () => {
+  console.log(`app is listening on the port number ${port}`);
+});
